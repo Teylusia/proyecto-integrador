54bad815b97815656b45b091f4fb6300cf63faa6
Teylusia <ataylor@udesa.edu.ar>
2021-06-22 16:31:54 -0300

    terminado el header y footer

--- js/header&footer.js
+++ js/header&footer.js
@@ -2,9 +2,11 @@ let buscador = document.querySelector(".buscador")
 let barra   = document.querySelector(".barra")
 buscador.addEventListener("submit", function(event){
     event.preventDefault();
-    if ( barra.value < 3 ) {
-        alert("Debes ingresar mas de 3 caracteres!");
-    } else {
-        this.submit()
+    if ( barra.value == 0) {
+        alert("No puedes dejar la casilla en blanco!");
+    } else if ( barra.value.length <= 3) {
+        alert("Debes escribir mas de 3 letras")
+    }else{
+        buscador.submit()
     }
 })
