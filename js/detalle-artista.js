7db59c0090b8a68a10bdb1b8639761fc19bf4450
Teylusia <ataylor@udesa.edu.ar>
2021-06-23 17:50:51 -0300

    detalle-artista

---
+++ js/detalle-artista.js
@@ -0,0 +1,24 @@
+let objeto = new URLSearchParams(location.search);
+let id = objeto.get('id');
+let artista = objeto.get('artist');
+let detalleArtista = document.querySelector('.detalle-artista')
+let imagen = document.querySelector('.foto-artista');
+let nombre= document.querySelector('.nombre-artista');
+let albums = document.querySelector('.albums');
+
+detalleArtista.innerHTML += artista
+
+fetch( `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}`)
+.then(response =>{
+    return response.json()
+})
+.then (dataArtista =>{
+    console.log(dataArtista);
+    let contenedor = document.querySelector('.detalle-artista')
+    for(i=0; i<3; i++){
+    contenedor.innerHTML +=
+    `
+    <img class="foto-artista" src="${artista.picture_big}" alt="${artista.name}"></img>
+        <h2 class="nombre-artista">${artista.name}</h2>
+    `}
+})
