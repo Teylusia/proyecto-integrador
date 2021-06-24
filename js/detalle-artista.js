201962cfe87859e7bf6672014d0ff0f34c3e55b3
Teylusia <ataylor@udesa.edu.ar>
2021-06-24 18:26:34 -0300

    detalle de artista hecho final

--- js/detalle-artista.js
+++ js/detalle-artista.js
@@ -4,16 +4,40 @@ console.log(id)
 let nombre = objeto.get("name");
 let  imagen = objeto.get("picture");
 let  detalleArtista = document.querySelector(".detalle-artista");
+let albumsArtista = document.querySelector(".albums");
+
 fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/" + id)
 .then( respuesta=>{
    return respuesta.json()
 })
+
 .then(dataArtistas =>{
     console.log(dataArtistas)
     let artists = dataArtistas
     detalleArtista.innerHTML += 
     `
-    <img class="imagen" src="${artists.picture_xl}" alt="${artists.name}">
+    <img class="imagen-artista" src="${artists.picture_xl}" alt="${artists.name}">
         <h3 class="nombre">${artists.name}</h3>
     `
+})
+
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/albums`)
+.then( respuesta =>{
+    return respuesta.json()
+})
+
+.then(dataArtistasAlbums =>{
+    console.log(dataArtistasAlbums)
+    
+    let albums = dataArtistasAlbums.data
+
+    for( i = 0; i < 3; i++){
+
+    albumsArtista.innerHTML +=
+    `
+
+        <img class="foto-album" src= ${albums[i].cover_xl} alt = ${albums[i].title}>
+        <h3><a class="album-nombre" href="detalle-album.html" target="_blank">${albums[i].title}</a></h3>
+    `
+    }
 })
