523f14790bc9d388a655818b92619de1b1132084
martumoroni <80982224+martumoroni@users.noreply.github.com>
2021-06-25 07:50:25 -0300

    y sigue pero meno

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,32 +1,6 @@
-window.addEventListener("load", function(){
-    let cancionesStorage = localStorage.getItem("playlist")
-    let playlist = JSON.parse(cancionesStorage) //pasarlo a JSON para manipularla
-    let listaCanciones = document.querySelector(".songname")
+let fotuli = document.querySelector(".fotuli")
+let datos = document.querySelector(".datos")
 
-    if (playlist){
-        playlist = []
-        listaCanciones.innerHTML += 
-        `
-        <h1 class = "vacio"> No has seleccionado ninguna canción aún</h1>
+let favoritasSongs = localStorage.getItem("favoritas")
+let arrayCanc = JSON.parse (favoritasSongs)
 
-        `
-    }else{
-        playlist.forEach(cancion =>{
-            listaCanciones.innerHTML +=
-            `
-            <div class = "details">
-            <div class = "numbername"> 
-            <a href = "detalle-cancion.html?id=${cancion.id}"<
-
-            <p class="num">${ca}
-
-
-
-            `
-        }
-            
-            
-            )
-    }
-
-})
